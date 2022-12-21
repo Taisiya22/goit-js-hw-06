2b92fb52dbde366378c5891ef4da3aaf7441fca9
Taisiya Tomashevska <taya22011985@gmail.com>
2022-12-21 22:03:02 +0200

    correct 2

--- js/task-02.js
+++ js/task-02.js
@@ -11,17 +11,20 @@ const ingredients = [
 const list = document.querySelector("#ingredients");
 
 
-
-
-
-
-ingredients.forEach((ingredient) => { 
-  const listElements = document.createElement("li");
-  listElements.textContent = ingredient;
-  listElements.classList.add('item');
- list.append(listElements);
+// ingredients.forEach((ingredient) => { 
+//   const listElements = document.createElement("li");
+//   listElements.textContent = ingredient;
+//   listElements.classList.add('item');
+//  list.append(listElements);
   
-})
+// })
 
 
-
+list.append(
+  ...ingredients.map((ingredient) => {
+    let listElements = document.createElement("li");
+    listElements.textContent = ingredient;
+    listElements.classList.add('item');
+    return listElements;
+  })
+);
